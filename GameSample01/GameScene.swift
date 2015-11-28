78a064a26ff85277bb5e4712953f463fc726c2a0
Tkray <tkra.yuuki@gmail.com>
2015-11-28 13:57:36 +0900

    second commit

---
+++ GameSample01/GameScene.swift
@@ -0,0 +1,11 @@
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    /// Sceneが表示された時に呼ばれる
+    override func didMoveToView(view: SKView) {
+        
+        // Sceneが呼ばれた時の処理を記載する
+        
+    }
+}
